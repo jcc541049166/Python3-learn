1eb6338bed8efe335cc43c61a39cab92f2d168b6
neo <nice_neo_cisco@hotmail.com>
2016-12-26 14:12:07 +0800

    add property.py file

---
+++ property.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3.4
+# -*- coding:utf-8 -*-
+
+class num(object):
+    def __init__(self,value):
+        self.value = value
+    def getNeg(self):
+        return -self.value
+    def setNeg(self,value):
+        self.value = -value
+    def delNeg(self):
+        print ("value alse deleted")
+        del self.value
+    neg = property(getNeg,setNeg,delNeg,"I'm negative")
+
+x = num(1.1)
+print (x.neg)
+x.neg = -22
+print (x.value)
+print (num.neg.__doc__)
+del x.neg
